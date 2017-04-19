b2a32e0aed65d62c99e1800626af34e8c36615ba
SreenivasulaReddy <sreenivasulareddy@vlsys61-130.local>
2017-04-19 14:47:13 +0530

    Initial Commit

---
+++ SreeMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SreeMenu
+//
+//  Created by SreenivasulaReddy on 19/04/17.
+//  Copyright © 2017 SreenivasulaReddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
